94afe852413ffa4ff9a1eaf462dca3c63a9dc18f
basnetutsav <basnetutsav338@gmail.com>
2025-11-02 23:58:06 -0800

    Add files via upload

--- asiignment2.0.py
+++ asiignment2.0.py
@@ -26,7 +26,7 @@ from sklearn.cluster import KMeans
 from collections import Counter
 import nltk
 
-
+# Download required NLTK data
 nltk.download('punkt')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
